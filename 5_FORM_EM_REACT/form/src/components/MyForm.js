7d307f75b987aa9c1d04a23fd2dad6372e36ac3a
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2025-03-27 10:34:11 -0300

    retornando os estudos

---
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -0,0 +1,17 @@
+import './MyForm.css'
+
+const MyForm = () => {
+  return (
+    <div>
+      {/* 1 - criacao de form */}
+      <form>
+        <div>
+            <label htmlFor="name">Nome:</label>
+            
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
